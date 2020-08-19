d53170288208213abcfc50baece894464363234e
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-08-19 18:27:20 +0100

    push

--- config/swarmConfig.sh
+++ config/swarmConfig.sh
@@ -8,5 +8,5 @@ export SWARM_VOL="demmon_volume"
 export DOCKER_IMAGE="nmmorais/demmon:latest"
 export CONFIG_FILE="config/config100.txt"
 
-export LATENCY_MAP="config/inet100Latencies_x0.005"
+export LATENCY_MAP="config/inet100Latencies_x0.005.txt"
 export IPS_MAP="config/ips100.txt"
--- scripts/buildImage.sh
+++ scripts/buildImage.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+printenv
+
 docker build --build-arg LISTEN_PORT --build-arg LATENCY_MAP --build-arg IPS_MAP -f build/Dockerfile -t nmmorais/demmon:latest .
