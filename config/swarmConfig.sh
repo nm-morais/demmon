78759803fe095977c4e9fcc497645ff499592a20
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-08-19 11:39:53 +0100

    convert to config file

---
+++ config/swarmConfig.sh
@@ -0,0 +1,6 @@
+# ./scripts/setupSwarm.sh 10.10.0.0/16 10.10.0.1 demmon_network demmon_volume
+
+export SWARM_GATEWAY="10.10.0.1"
+export SWARM_SUBNET="10.10.0.0/16"
+export SWARM_NET="demmon_network"
+export SWARM_VOL="demmon_volume"
--- scripts/setupSwarm.sh
+++ scripts/setupSwarm.sh
@@ -2,14 +2,7 @@
 
 # Credit : https://github.com/pedroAkos
 
-
-subnet=$1
-gateway=$2
-name=$3
-volume=$4
-
-
-if [ -z $subnet ] || [ -z $gateway ] || [ -z $name ] || [ -z $volume ]; then
+if [ -z $SWARM_SUBNET ] || [ -z $SWARM_GATEWAY ] || [ -z $SWARM_NET ] || [ -z $SWARM_VOL ]; then
   echo "setup needs exactly 4 arguments"
   echo "source setup.sh <subnet> <gateway> <net_name> <volume_name>"
   exit
@@ -17,7 +10,6 @@ fi
 
 docker swarm init
 JOIN_TOKEN=$(docker swarm join-token manager -q)
-
 host=$(hostname)
 for node in $(oarprint host); do
   if [ $node != $host ]; then
@@ -25,8 +17,5 @@ for node in $(oarprint host); do
   fi
 done
 
-docker volume create $volume
-docker network create -d overlay --attachable --subnet $subnet --gateway $gateway $name
-
-export DOCKER_NET="$name"
-export DOCKER_VOL="$volume"
+docker volume create $SWARM_VOL
+docker network create -d overlay --attachable --subnet $SWARM_SUBNET --gateway $SWARM_GATEWAY $SWARM_NET
