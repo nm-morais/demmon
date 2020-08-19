8a4d2c81c64ceaa62609ddcfc35f7f3267507474
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-08-19 12:02:16 +0100

    update scripts

--- scripts/setupContainers.sh
+++ scripts/setupContainers.sh
@@ -63,8 +63,8 @@ do
   esac
 
   node=$(nextnode $i)
-  echo "oarsh $node 'docker run --rm -v ${SWARM_VOL}:/code/logs -d -t --cpus=$cpu --cap-add=NET_ADMIN --net $net --ip $ip --name $name -h $name $image $i'"
-  oarsh -n $node "docker run --rm -v ${SWARM_VOL}:/code/logs -d -t --cpus=$cpu --cap-add=NET_ADMIN --net $net --ip $ip --name $name -h $name $image $i"
+  echo "oarsh $node 'docker run --rm -v $SWARM_VOL:/code/logs -d -t --cpus=$cpu}} --cap-add=NET_ADMIN --net $net --ip $ip --name $name -h $name $DOCKER_IMAGE $i'"
+  oarsh -n $node "docker run --rm -v $SWARM_VOL:/code/logs -d -t --cpus=$cpu --cap-add=NET_ADMIN --net $net --ip $ip --name $name -h $name $DOCKER_IMAGE $i"
   echo "${i}. Container $name with ip $ip lauched"
   i=$((i+1))
 done < "$CONFIG_FILE"
