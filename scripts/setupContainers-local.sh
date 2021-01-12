06b79f0e0b9de6e109db85f145bc3f61254c047a
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-01-12 15:42:00 +0000

    fix tc establishing delays to all nodes in ips / latencies file

--- scripts/setupContainers-local.sh
+++ scripts/setupContainers-local.sh
@@ -3,13 +3,14 @@
 set -e
 
 ./scripts/buildImage.sh
+nContainers=$(wc -l $CONFIG_FILE)
 
 i=0
 echo "Lauching containers..."
 while read -r layer ip name
 do
   echo "$layer $ip $name"
-  docker run -v $SWARM_VOL:/tmp/logs -itd --cap-add=NET_ADMIN --net $SWARM_NET --ip $ip --name $name -h $name $DOCKER_IMAGE $i "$@"
+  docker run -v $SWARM_VOL:/tmp/logs -itd --cap-add=NET_ADMIN --net $SWARM_NET --ip $ip --name $name -h $name $DOCKER_IMAGE $i $nContainers "$@"
   echo "${i}. Container $name with ip $ip lauched"
   i=$((i+1))
   sleep 1.5
