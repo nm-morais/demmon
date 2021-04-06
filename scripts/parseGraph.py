0fc4e85568ca0f17c87058cd24a8d02224246554
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-04-06 14:07:15 +0100

    changes in latency map generation file

--- scripts/parseGraph.py
+++ scripts/parseGraph.py
@@ -2,18 +2,19 @@ from typing import ForwardRef
 from graph_tool.all import *
 import math
 import csv
+from tqdm import tqdm
 
 
 def cartesian_dist(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 
-def can_add(g, v, added):
-    p = (g.vp.lat[v], g.vp.long[v])
-    for a in added:
-        if cartesian_dist(p, a) < 0.01:
-            return False
-    return True
+def already_added(g, v, added_map):
+    # p = (g.vp.lat[v], g.vp.long[v])
+    # for k in added_map:
+    #     if cartesian_dist(p, added_map[k]) < 0.001:
+    #         return k
+    return None
 
 
 # Cada vertice tem as seguintes propriedades:
@@ -49,36 +50,53 @@ def can_add(g, v, added):
 
 if __name__ == "__main__":
 
-    p1 = (2, 4)
-    p2 = (1, 2)
-
     # faz load do graph para memoria
     g = load_graph("config/graph.xml.gz")
     # Vais buscar a maior component conexa que é o dos 8mil
-    g = GraphView(g, efilt=lambda e: g.ep.lat[e] < 100)
+    g.list_properties()
 
     g = GraphView(g, vfilt=label_largest_component(g))
-
     added_map = {}
-    added = []
     conf_f = open("config/coords_file.txt", 'w', newline='')
     lat_f = open("config/lats_file.txt", 'w', newline='')
     conf_writer = csv.writer(conf_f, delimiter=" ")
     lats_writer = csv.writer(lat_f, delimiter=" ")
+    edges_to_add = []
+    for v in g.iter_vertices():
+        # print(v)
+        # node_already_added = already_added(g, v, added_map)
+        # if node_already_added is not None:
+        # print("already added:", node_already_added)
+        # print(g.ep.lat)
+        # g.ep.lat[new_edge] = g.ep.lat[(v, v2)]
+        # print(
+        #     f"Skipping node {v} with lat: {g.vp.lat[v]}, lon:{g.vp.long[v]}")
+        # exit()
+        # else:
+        #     added_map[v] = (g.vp.lat[v], g.vp.long[v])
 
-    for v in g.vertices():
-        if not can_add(g, v, added):
-            print(
-                f"Skipping node {v} with lat: {g.vp.lat[v]}, lon:{g.vp.long[v]}")
-            continue
-        added.append((g.vp.lat[v], g.vp.long[v]))
-        added_map[v] = True
+        for e in g.iter_out_edges(v, eprops=[g.ep.lat]):
+            edges_to_add.append((e[0], e[1],  e[2] * 0.3))
 
-    for i, v in enumerate(added_map):
+    print("adding edge list")
+    # print(edges_to_add)
+
+    g.add_edge_list(edges_to_add, eprops=[g.ep.lat])
+    g = GraphView(g, efilt=lambda e: g.ep.lat[e] < 50)
+    g = GraphView(g, vfilt=label_largest_component(g))
+    n_nodes = 0
+    for v in g.iter_vertices():
+        n_nodes += 1
+    print("filtering by edges")
+    print("getting shortest distances...")
+    pbar = tqdm(total=n_nodes)
+    for v in g.iter_vertices():
         dist_map = shortest_distance(
-            g, source=v, target=[_v for _v in added_map], weights=g.ep.lat)
+            g, source=v, target=[_v for _v in g.iter_vertices()], weights=g.ep.lat)
         lats_writer.writerow(['{:.2f}'.format(x) for x in dist_map])
         conf_writer.writerow([g.vp.lat[v], g.vp.long[v]])
+        pbar.update(1)
+    pbar.close()
 
     print(f"Wrote coords file to: config/coords_file.txt")
     print(f"Wrote coords file to: config/lats_file.txt")
