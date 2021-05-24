c7b8d951182c6a15b96020d46ef4a49f641c91f4
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-05-24 21:06:25 +0100

    bugfix

--- scripts/syncFilesGrid.sh
+++ scripts/syncFilesGrid.sh
@@ -4,9 +4,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-
-
-run_rsync='rsync --progress -razPv --exclude 'demmon_logs' --exclude 'thesis_evaluation/stats' --exclude 'thesis_evaluation/config/ips_file.txt' --exclude 'histogram.png' /Users/nunomorais/go/src/github.com/nm-morais/ nantes.g5k:/home/nmorais/git/nm-morais'
+run_rsync='rsync --progress -razPv --exclude 'demmon_logs' --exclude 'thesis_evaluation/stats' --exclude 'thesis_evaluation/config/ips_file.txt' --exclude 'histogram.png' /Users/nunomorais/go/src/github.com/nm-morais/ $1.g5k:/home/nmorais/git/nm-morais'
 eval $run_rsync; fswatch -or . | while read f; do eval $run_rsync; done
 
 # rsync -rv /Users/nunomorais/go/src/github.com/nm-morais/ dicluster:/home/nunomorais/git/nm-morais
